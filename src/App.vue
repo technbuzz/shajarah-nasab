11dfeb6b7511cb01c28c3560a374733f7779b4e8
technbuzz <samiullah.khan.1989@gmail.com>
2023-05-23 23:36:15 +0400

    added styles and rendered person

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { reactive, ref } from 'vue'
+import PersonName from './components/PersonName.vue'
 
-    const state = ref(0)
     const person = ref(null)
-fetch('data.json').then(resp => resp.json()).then(data => person.value = data )
+  fetch('data.json').then(resp => resp.json()).then(data => person.value = data )
 
 
-    console.log(state)
 
 </script>
 
 <template>
-  <header>
+  <header class="text-center">
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <!---->
     <!-- <div class="wrapper"> -->
@@ -26,11 +25,18 @@ fetch('data.json').then(resp => resp.json()).then(data => person.value = data )
       <h2 class="text-xl my-1 mb-6 font-bold">سکن گاون ۔ کنگانہ۔ قوم شیخان ۔ تپہ سموزی</h2>   <!-- </div> -->
   </header>
   <ul class="person-list mt-4">
-    <li v-for="item in person">{{ item.name.en.firstName}}</li>
-
+    <li v-for="item in person">
+      <div class="person text-center">
+        <img class="m-auto" src="@/assets/user.png" width="64">
+        <!-- <h3 class="person__name font-bold text-lg" v-if="item.name.ur"> -->
+        <!--   {{ur.title}} {{ur.firstName}} {{ur.lastName}} -->
+        <!-- </h3> -->
+        <PersonName :name="item.name.ur" />
+        <p class="text-slate-400">{{item.description.ur}}</p>
+      </div>
+    </li>
   </ul>
 
-  <pre> state {{ state}} </pre>
   <RouterView />
 </template>
 
---
+++ src/components/PersonName.vue
@@ -0,0 +1,9 @@
+<script setup>
+  defineProps(['name'])
+</script>
+
+<template>
+  <h3 class="person__name font-bold text-lg" >
+    {{name.title}} {{name.firstName}} {{name.lastName}}
+  </h3>
+</template>
