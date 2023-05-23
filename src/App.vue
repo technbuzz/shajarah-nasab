1469f0ba95df7a54576e5c3d703812868f62e0de
technbuzz <samiullah.khan.1989@gmail.com>
2023-05-24 00:36:49 +0400

    added hosting

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@ import PersonName from './components/PersonName.vue'
 
 <template>
   <header class="text-center">
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <!---->
     <!-- <div class="wrapper"> -->
     <!--   <HelloWorld msg="You did it!" /> -->
